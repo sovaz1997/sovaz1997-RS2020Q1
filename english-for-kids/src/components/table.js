e2258b522a4abb2dcd97e8d516b274d3e291ef35
Oleg Smirnov <sovaz1997@yandex.ru>
2020-04-19 16:03:44 +0300

    feat: add table sort

--- english-for-kids/src/components/table.js
+++ english-for-kids/src/components/table.js
@@ -5,6 +5,7 @@ export default class Table {
     fields: [],
     data: [],
     sortIndex: 0,
+    ascending: true,
   };
 
   constructor() {
@@ -17,5 +18,28 @@ export default class Table {
 
   render() {
     this.el.innerHTML = '';
+
+    this.sortData();
+  }
+
+  sortData() {
+    const { sortIndex } = this.state;
+    const { sortType } = this.state.fields[sortIndex];
+    this.state.data = this.state.data.sort((a, b) => {
+      let res = 0;
+      if (sortType === 'number') {
+        res = Number(b[sortIndex]) - Number(a[sortIndex]);
+      } else if (sortType === 'string') {
+        if (a < b) res = 1;
+        else if (a > b) res = -1;
+        else res = 0;
+      }
+
+      return res;
+    });
+  }
+
+  addField(name, text, sortType) {
+    this.state.fields.push({ name, text, sortType });
   }
 }
