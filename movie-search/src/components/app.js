baa5c21766bd16e16fe65b3c61cf0a50e601abcd
sovaz1997 <sovaz1997@yandex.ru>
2020-04-29 19:22:30 +0300

    feat: create header

--- movie-search/src/components/app.js
+++ movie-search/src/components/app.js
@@ -1,20 +1,23 @@
 import Utils from '../utils/utils';
 
 export default class App {
-  state = {
-    pages: {},
-  };
-
   constructor() {
     this.createElement();
+    this.render();
   }
 
-
   createElement() {
-    this.el = Utils.createElement('div', 'app');
+    this.el = Utils.createElement('div', ['app']);
+  }
+
+  static createHeader() {
+    return Utils.createElement('header', ['site-header'], [
+      Utils.createElement('h1', ['site-header__name'], ['Movie Search']),
+    ]);
   }
 
   render() {
     this.el.innerHTML = '';
+    this.el.append(App.createHeader());
   }
 }
--- movie-search/src/utils/utils.js
+++ movie-search/src/utils/utils.js
@@ -1,8 +1,18 @@
 export default class Utils {
-  static createElement(tagName, classList) {
+  static createElement(tagName, classList, childs) {
     const el = document.createElement(tagName);
 
-    if (classList) el.className = classList.split(' ');
+    if (classList) el.className = classList.join(' ');
+
+    if (childs) {
+      childs.forEach((child) => {
+        if (typeof child === 'string') {
+          el.append(document.createTextNode(child));
+        } else {
+          el.append(child);
+        }
+      });
+    }
 
     return el;
   }
